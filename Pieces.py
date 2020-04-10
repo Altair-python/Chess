a9cf70d06e8499083b437b78c9ec39df73b479f8
Altair-python <54147755+Altair-python@users.noreply.github.com>
2020-04-10 16:16:41 +0530

    Update Pieces.py

--- Pieces.py
+++ Pieces.py
@@ -21,7 +21,7 @@ def check(gb,current,side):
         for k in j:
             if i.side !=k.side and k.side != 'nothing':
                 if k.isPossible(i.y,i.x,GB,current):
-                    return f'Check by {k.side} {k.shape}'
+                    return f'Check by {k.side} {k.shape} {k.x , k.y}'
 
 
 def win(gb):
@@ -52,8 +52,56 @@ def createp(side):
 def CreateGB():
     return createp('black') + [[Null() for _ in range(8)] for _ in range(4)] + createp('white')
 
+def vertical(y1,y2,x,gb):
+    z=1
+    if y2-y1 < 0:
+        y1, y2= y2,y1
+        
+    #flag=True
+    #print(str((x1,y1)) + ' to ' + str((x2,y2)))
+    for i in range(y1+z,y2):
+        if gb[i][x].side != 'nothing':
+            print('Error')
+            return False
+    return True
 
+def horizontal(x1,x2,y,gb):
+    z=1
+    if x2-x1 < 0:
+        x1, x2= x2,x1
+    print(str((x1,y1)) + ' to ' + str((x2,y2)))
+    for i in range(x1+z,x2):
+        if gb[y][i].side != 'nothing':
+            print('Error')
+            return False
+    return True
 
+def diagonal(x1,y1,x2,y2,gb):
+    
+    if x2-x1 > 0 and y2-y1>0:
+        kx=ky=1     
+        
+    elif x2-x1 > 0 and y2-y1<0:
+        kx=1
+        ky=-1
+        
+    elif x2-x1 < 0 and y2-y1<0:
+        kx=-1
+        ky=-1
+        
+    elif x2-x1 < 0 and y2-y1>0:
+        kx=-1
+        ky=1
+    distance = pow((abs(y2 - y1)**2 + abs(x2 - x1)**2 )/2,0.5)
+    print(distance.is_integer())
+    if distance.is_integer():
+        for i in range(1,int(distance)):
+            if gb[y1 + ky*i][x1 + kx*i].side !='nothing':
+                print('Error')
+                return False
+        return True
+    return False
+        
 class Null:
     def __init__(self):
         self.side='nothing'
@@ -93,17 +141,21 @@ class Pawn(BasicPieces):
 
     def isPossible(self,y,x,GB,current):
         if 0 <= x < 8 and 0 <= y < 8:
-            if  abs(self.y - y) == 2 and abs(self.x - x) == 0:
-                if self.moves == 1:
+            if  abs(y-self.y) == 2 and abs(self.x - x) == 0:
+                print('True')
+                if self.moves == 1 and vertical(self.y,y,x,GB):
+                    self.moves+=1
+                    print('True')
+                    return True
+                return False
+            
+            elif abs(y-self.y) == 1 and abs(self.x - x) == 0:
+                if GB[y][x].side == 'nothing':
                     self.moves+=1
                     return True
                 return False
             
-            elif abs(self.y - y) == 1 and abs(self.x - x) == 0:
-                self.moves+=1
-                return True
-            
-            elif abs(self.y - y) ==1 and abs(self.x - x) == 1:
+            elif abs(y-self.y) ==1 and abs(self.x - x) == 1:
                 if GB[y][x].side not in ['nothing',self.side]:
                     self.moves+=1
                     return True
@@ -139,9 +191,9 @@ class Bishop(BasicPieces):
     def isPossible(self,y,x,GB,current):
         if 0 <= x < 8 and 0 <= y < 8:
             self.distance=pow((abs(self.y - y)**2 + abs(self.x - x)**2 )/2,0.5)
-            if self.distance.is_integer():
+            if pow((abs(self.y - y)**2 + abs(self.x - x)**2 )/2,0.5).is_integer():
                 #print(y,x)
-                if GB[y][x].side != self.side:
+                if GB[y][x].side != self.side and diagonal(self.x,self.y,x,y,GB):
                     return True
             return False
         return False
@@ -157,11 +209,11 @@ class Rook(BasicPieces):
     def isPossible(self,y,x,GB,current):
         if 0 <= x < 8 and 0 <= y < 8:
             if  abs(self.y - y) and abs(self.x - x) == 0:
-                if GB[y][x].side != self.side:
+                if GB[y][x].side != self.side and vertical(self.y,y,x,GB):
                     return True
             
             elif abs(self.y - y) == 0 and abs(self.x - x):
-                if GB[y][x].side != self.side:
+                if GB[y][x].side != self.side and horizontal(self.x,x,y,GB):
                     return True
                 
             return False
@@ -178,15 +230,15 @@ class Queen(BasicPieces):
     def isPossible(self,y,x,GB,current):
         if 0 <= x < 8 and 0 <= y < 8:
             if  abs(self.y - y) and abs(self.x - x) == 0:
-                if GB[y][x].side != self.side:
+                if GB[y][x].side != self.side and vertical(self.y,y,x,GB):
                     return True
             
             elif abs(self.y - y) == 0 and abs(self.x - x):
-                if GB[y][x].side != self.side:
+                if GB[y][x].side != self.side and horizontal(self.x,x,y,GB):
                     return True
             elif pow((abs(self.y - y)**2 + abs(self.x - x)**2 )/2,0.5).is_integer():
                 #print(y,x)
-                if GB[y][x].side != self.side:
+                if GB[y][x].side != self.side and diagonal(self.x,self.y,x,y,GB):
                     return True
             return False
         return False
@@ -234,10 +286,10 @@ while play:
         played=False
         p()
         while not played:
-            x1=int(input(''))
-            y1=int(input(''))
-            x2=int(input(''))
-            y2=int(input(''))
+            x1=int(input('X1 > '))
+            y1=int(input('Y1 > '))
+            x2=int(input('X2 > '))
+            y2=int(input('Y2 > '))
             if GB[y1][x1].side == current_player:
                 if m(x1,y1,x2,y2,moves[current_player]):
                     break
